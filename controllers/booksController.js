55c6a65bbeafddfb8ccd5e5483caceff725b3fbb
Julia Pauletti <julia.pauletti@yahoo.com>
2019-07-24 14:23:45 -0500

    code cleanup, beginning of readme

--- controllers/booksController.js
+++ controllers/booksController.js
@@ -2,20 +2,20 @@ const db = require("../models");
 
 // Defining methods for the booksController
 module.exports = {
+    getAll: function (req, res) {
+        db.Book.find({})
+        .then(function (dbData) {
+            res.json(dbData);
+            // console.log(dbData);
+        }).catch(err => { if (err) console.log(err) })
+    },
+
     save: function (req, res) {
         db.Book.create(req.body)
         .then(function (dbData) {
             console.log(dbData);
             res.json(dbData);
         }).catch(err => { if (err) console.log(err) });
-    },
-
-    getAll: function (req, res) {
-        db.Book.find({})
-        .then(function(dbData) {
-            res.json(dbData);
-            // console.log(dbData);
-        }).catch(err => { if(err) console.log(err) })
     },
 
     delete: function(req, res) {
@@ -26,15 +26,4 @@ module.exports = {
             res.json(dbData);
         }).catch(err => { if (err) console.log(err) });
     }
-
-
-    // router.route("/api/books/:id", (req, res) => {
-    //     // delete a book from the database by Mongo _id
-    //     db.Book.findOneAndDelete({ _id: req.params.id })
-    //         .then(function (dbData) {
-    //             console.log(dbData)
-    //             res.json(dbData);
-    //         }).catch(err => { if (err) console.log(err) });
-    // })
-
 };
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -5,44 +5,13 @@ const booksController = require("../controllers/booksController");
 
 router.route("/api/books").get(booksController.getAll);
 
-// router.route("/api/books", (req, res) => {
-//     // save a new book to the database
-//     db.Book.save(req.body).then(function(dbData) {
-//         console.log("saving a new book....hopefully");
-//         console.log(req.body);
-//         console.log(dbData);
-//     }).catch(err => { if (err) console.log(err) });
-// })
-
 router.route("/api/books").post(booksController.save);
 
 router.route("/api/books/:id").delete(booksController.delete);
 
-// router.route("/api/books/:id", (req, res) => {
-//     // delete a book from the database by Mongo _id
-//     db.Book.findOneAndDelete({ _id: req.params.id })
-//     .then(function (dbData) {
-//         console.log(dbData)
-//         res.json(dbData);
-//     }).catch(err => { if (err) console.log(err) });
-// })
-
 router.route("*", (req, res) => {
     // load your single HTML page in 'client/build/index.html'
+    res.sendFile(path.join(__dirname, "./index.html"));
 })
 
 module.exports = router;
-
-// // Matches with "/api/books"
-// router.route("/")
-//     .get(booksController.findAll)
-//     .post(booksController.create);
-
-// // Matches with "/api/books/:id"
-// router
-//     .route("/:id")
-//     .get(booksController.findById)
-//     .put(booksController.update)
-//     .delete(booksController.remove);
-
-// module.exports = router;
